0e16343a7c1403eb737064b1aa17ec4d7e7381e1
Chen H <charles_hou@jiliguala.com>
2021-01-21 05:34:38 +0800

    feat(application): add web application entry

---
+++ src/main/java/chen/vamdawn/binflare/Application.java
@@ -0,0 +1,18 @@
+package chen.vamdawn.binflare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * application entry
+ *
+ * @author chen
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
