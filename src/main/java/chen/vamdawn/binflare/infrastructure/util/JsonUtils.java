6f0929e91e193a5c921911eea52841d929b4e7f0
Chen H <charles_hou@jiliguala.com>
2021-02-15 19:07:50 +0800

    feat(JsonUtils): let jackson serialize date to iso date format

--- src/main/java/chen/vamdawn/binflare/infrastructure/util/JsonUtils.java
+++ src/main/java/chen/vamdawn/binflare/infrastructure/util/JsonUtils.java
@@ -2,6 +2,8 @@ package chen.vamdawn.binflare.infrastructure.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * JSON operation utilities based on Jasckson.
@@ -11,6 +13,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonUtils {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.registerModule(new JavaTimeModule());
+        MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
 
     private JsonUtils() {
         throw new UnsupportedOperationException("utility class should not be instantiated.");
